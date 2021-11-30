8c6cc28968c6e0cdf2d39a921a2dc407e24c3a24
PetarMuzurovic <petarmuzurovic@gmail.com>
2021-11-30 14:53:43 +0100

    server

--- Server.js
+++ Server.js
@@ -8,3 +8,18 @@ application.listen(port, () => {
     console.log('upaljeno');
 
 });
+
+application.use(express.json());
+
+const a = 1;
+
+application.get("/", (request, response) => {
+
+    response.json({
+
+        poruka: "ovo je stiglo od jsona",
+        vrednost: a,
+
+    });
+
+});
