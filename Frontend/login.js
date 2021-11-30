a3060e5f8e77b9879e36e192f53bd3f161cd6e63
Milan-Djuric <milan.djuric@prvabeogim.edu.rs>
2021-11-30 16:03:41 +0100

    js login

--- Frontend/login.js
+++ Frontend/login.js
@@ -3,10 +3,14 @@ function login(event){
     var user = document.getElementsByClassName("input")[0].value;
     var password = document.getElementsByClassName("input")[1].value;
 
-    if (user.length<=0) {
-        //prikazi username nije validan
-    } else if(password.length<=0) {
-        //prikazi password nije validan
+    var div = document.getElementById("box");
+    var prazni = document.getElementById("prazni");
+    var pogresni = document.getElementById("pogresni");
+    if (user.length<=0 || password.length<=0) {
+        //prikazi da nisu uneti podaci
+        pogresni.style.display = "none";
+        div.style.display = "block";
+        prazni.style.display = "block";
     } else {
         //ajax posalje username i password
     }
