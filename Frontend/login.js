65d03ce2809ade26e7b0178daa584520a5099bf1
Milan-Djuric <milan.djuric@prvabeogim.edu.rs>
2021-11-30 14:23:24 +0100

    js login

---
+++ Frontend/login.js
@@ -0,0 +1,5 @@
+function login(event){
+    event.preventDefault();
+    var user = document.getElementsByClassName("input")[0].value;
+    var password = document.getElementsByClassName("input")[1].value;
+}
