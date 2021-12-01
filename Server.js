13f47f6f3ae359624e9bbe41d56b01f4f548238e
PetarMuzurovic <petarmuzurovic@gmail.com>
2021-12-01 10:28:09 +0100

    flag za loggin

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const application = express();
 const Connect = require("./Database/Baza");
+const User = require("./Database/Users");
 const port = 1000;
 
 application.listen(port, () => {
@@ -15,13 +16,43 @@ application.use(express.json());
 
 application.use(express.static("Frontend"));
 
-const flag = false;
+var flag = 0;
 
-application.post("/User/Log", (request, response) => {
+application.post("/User/Log", async (request, response) => {
 
     try{
-        console.log("req pogodjen"),
-        console.log(request.body)
+        console.log("req pogodjen");
+
+        const AllUsers = await User.find();
+        /*const usern = "Hakaton2021";
+        const pass = "HZS4";
+        const newUser = new User({
+            Username: usern,
+            Password: pass,
+        });
+
+        const provera = await newUser.save();*/
+
+        /*console.log(AllUsers);*/
+        AllUsers.forEach(use => {
+            
+            var f = 0;
+
+            if (request.body.user == use.Username) {
+                if (request.body.password == use.Password) {
+                    f = 1;
+                }
+            }
+
+            if (f == 1) flag = 1;
+
+        });
+
+        //console.log(provera);
+        
+        response.json({
+            uspesno: flag,
+        });
     } catch(err) {
         response.status(404).json({
         poruka: err.message,
